765c697ff46ea49660726fa0fff9063723a333c4
Kendrick Tan <kendricktan0814@gmail.com>
2016-08-07 01:05:29 +1000

    ajax submit

--- signups/views.py
+++ signups/views.py
@@ -1,5 +1,6 @@
 from django.views import View
 from django.shortcuts import render
+from django.http import JsonResponse
 
 from signups.forms import LandingForm
 
@@ -16,8 +17,10 @@ class LandingView(View):
         form = LandingForm(request.POST)
         if form.is_valid():
             form.save()
+            context['success'] = True
             context['message'] = "Success! We will notify you when we're live!"
         else:
+            context['success'] = False
             context['message'] = "This email address has already been submited."
 
-        return render(request, 'landing.html', context)
+        return JsonResponse(context)
