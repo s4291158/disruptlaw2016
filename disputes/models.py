f1dd542c9c820224493f9e4f1970de3f51d02b85
Zerong Tony Wang <ztony_wang@hotmail.com>
2016-08-07 02:36:01 +1000

    created questions

--- disputes/models.py
+++ disputes/models.py
@@ -1,6 +1,9 @@
 from django.db import models
-
 from disputes.states_table import STATES
+
+from disputes.category_model import DisputeCategory, DisputeCategoryQuestion
+
+import random
 
 
 class Dispute(models.Model):
@@ -18,12 +21,33 @@ class Dispute(models.Model):
         blank=True
     )
 
-    title = models.CharField(max_length=40)
+    alt_id = models.IntegerField(
+        default=0,
+        unique=True
+    )
+    title = models.CharField(
+        max_length=40,
+        default=''
+    )
     times_created = models.DateTimeField(auto_now_add=True)
     state = models.IntegerField(
         choices=STATES,
         default=1
     )
+
+    def __init__(self, *args, **kwargs):
+        super(self.__class__, self).__init__(*args, **kwargs)
+        self.make_alt_id()
+        self.title = str(self.alt_id)
+
+    def make_alt_id(self, length=8):
+        lower = 10 ** (length - 1)
+        upper = 10 ** length - 1
+
+        alt_id = random.randint(lower, upper)
+        while self.objects.filter(alt_id=alt_id).count() > 0:
+            alt_id = random.randint(lower, upper)
+        self.alt_id = alt_id
 
 
 class DisputeMaterial(models.Model):
