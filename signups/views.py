297916413fddb8ae53c6cda32c3ec0eb258c1172
Zerong Tony Wang <ztony_wang@hotmail.com>
2016-08-06 22:44:24 +1000

    landing working

--- signups/views.py
+++ signups/views.py
@@ -6,9 +6,18 @@ from signups.forms import LandingForm
 
 class LandingView(View):
     def get(self, request):
-        return render(request, 'landing.html', {})
+        context = {
+            'form': LandingForm()
+        }
+        return render(request, 'landing.html', context)
 
     def post(self, request):
+        context = {}
         form = LandingForm(request.POST)
         if form.is_valid():
             form.save()
+            context['message'] = "Success! We will notify you when we're live!"
+        else:
+            context['message'] = "This email address has already been submited."
+
+        return render(request, 'landing.html', context)
