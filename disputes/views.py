6ddfb0417d6537cac2e5b558c139a09da7ed6fe4
Kendrick Tan <kendricktan0814@gmail.com>
2016-08-06 21:59:38 +1000

    landing

--- disputes/views.py
+++ disputes/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 def index_view(request):
     return render(request, 'index.html', {})
 
+def landing(request):
+    return render(request, 'landing.html', {})
+
 def resolve(request):
     return render(request, 'resolve.html', {})
 
