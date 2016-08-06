9ea469c8f1638f540df430832a628e1343404f07
Kendrick Tan <kendricktan0814@gmail.com>
2016-08-06 19:53:45 +1000

    top

--- forum/urls.py
+++ forum/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
-from forum.views import index_view
+from forum.views import index_view, resolve, dispute
 
 urlpatterns = [
     url(r'^$', index_view, name='index_view'),
+    url(r'^dispute/?$', dispute, name='dispute'),
+    url(r'^resolve/?$', resolve, name='resolve'),
 ]
--- forum/views.py
+++ forum/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def index_view(request):
     return render(request, 'index.html', {})
+
+def resolve(request):
+    return render(request, 'resolve.html', {})
+
+def dispute(request):
+    return render(request, 'dispute.html', {})
