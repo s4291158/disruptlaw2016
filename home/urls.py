2f9bbb58613539e524e145f0c333b4bb763a3c30
Zerong Tony Wang <ztony_wang@hotmail.com>
2016-08-07 11:59:02 +1000

    fixed landing url

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from home.views import LandingView, IndexView
 
 urlpatterns = [
-    url(r'^landing/?$', LandingView.as_view(), name='landing_view'),
+    url(r'^landing?$', LandingView.as_view(), name='landing_view'),
     url(r'^$', IndexView.as_view(), name='index_view'),
 ]
